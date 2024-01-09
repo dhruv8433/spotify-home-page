14a7d8bfdf48915b11841b2beb7f44f73aa3a90e
dhruv8433 <dhruvrsoni04@gmail.com>
2024-01-09 21:45:12 +0530

    playlist card

--- app/components/SongCard.jsx
+++ app/components/SongCard.jsx
@@ -23,8 +23,8 @@ const SongCard = ({ song }) => {
           />
         </div>
         <div className="absolute bottom-32 right-4 opacity-0 group-hover:opacity-100">
-          <IconButton className="p-2 bg-green-600">
-            <PlayArrow />
+          <IconButton className="p-2 bg-green-600 hover:bg-green-600">
+            <PlayArrow sx={{ color: "black" }} />
           </IconButton>
         </div>
       </CardMedia>
