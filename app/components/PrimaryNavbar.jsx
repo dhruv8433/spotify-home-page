3cbb1dd62ba1e84bcd720f6c249eb5f5c76d95f5
dhruv8433 <dhruvrsoni04@gmail.com>
2024-01-09 18:10:10 +0530

    left nav

---
+++ app/components/PrimaryNavbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavComponent1 from "./NavComponent1";
+import NavComponent2 from "./NavComponent2";
+
+const PrimaryNavbar = () => {
+  return (
+    <>
+      <NavComponent1 />
+      <NavComponent2 />
+    </>
+  );
+};
+
+export default PrimaryNavbar;
