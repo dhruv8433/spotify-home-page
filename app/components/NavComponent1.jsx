bb7cf6d751ec2f71eb6c642aaa20ac58ce85a8fa
dhruv8433 <dhruvrsoni04@gmail.com>
2024-01-09 18:09:50 +0530

    left navigation components

---
+++ app/components/NavComponent1.jsx
@@ -0,0 +1,25 @@
+import { Home, Search } from "@mui/icons-material";
+import React from "react";
+
+const NavComponent1 = () => {
+  return (
+    <div className="primary-nav rounded-md">
+      <div className="mt-1 ml-1 p-6 rounded flex items-center">
+        <img src="/asset/spotify-logo.png" alt="logo" height={20} width={20} />
+        <h1 className="font-semibold ml-1">Spotify</h1>
+      </div>
+      <div className="options">
+        <div className="flex ml-6 my-2 hover:underline hover:cursor-pointer">
+          <Home />
+          <h1 className="ml-4">Home</h1>
+        </div>
+        <div className="flex ml-6 my-2 pb-2 hover:underline hover:cursor-pointer">
+          <Search />
+          <h1 className="ml-4">Search</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavComponent1;
---
+++ app/components/NavComponent2.jsx
@@ -0,0 +1,69 @@
+import { Add, LibraryMusic, Public } from "@mui/icons-material";
+import React from "react";
+import PlaylistCard from "./PlaylistCard";
+import Link from "next/link";
+
+const NavComponent2 = () => {
+  return (
+    <div className="primary-nav rounded-md h-[84vh]">
+      {/* your library */}
+      <div className="p-6">
+        <div className="library flex justify-between text-gray-400 hover:text-white">
+          <div className="flex items-center">
+            <LibraryMusic className=" " />
+            <h1 className="ml-1 font-semibold">Your Library</h1>
+          </div>
+          <div className="">
+            <Add />
+          </div>
+        </div>
+
+        {/* library cards */}
+        <PlaylistCard
+          title={"Create your first playlist"}
+          desc={"It's easy, we'll help you"}
+          btn={"Create playlist"}
+        />
+        <PlaylistCard
+          title={"Let's find some prodcasts to follow"}
+          desc={"We'll keep you updated on new episodes."}
+          btn={"Browse Prodcasts"}
+        />
+
+        {/* privacy center */}
+        <div className="mt-52">
+          <div className="flex justify-between">
+            <Link className="text-sm text-gray-300" href={"/"}>
+              Legal
+            </Link>
+            <Link className="text-sm text-gray-300" href={"/"}>
+              Privacy center
+            </Link>
+            <Link className="text-sm text-gray-300" href={"/"}>
+              Privacy policy
+            </Link>
+            <Link className="text-sm text-gray-300" href={"/"}>
+              Cookies
+            </Link>
+            <Link className="text-sm text-gray-300" href={"/"}>
+              About Ads
+            </Link>
+          </div>
+          <Link className="text-sm text-gray-300" href={"/"}>
+            Accessibility
+          </Link>{" "}
+          <br />
+          <Link className="text-sm text-gray-300" href={"/"}>
+            Cookies
+          </Link>
+          <div className="lang border border-gray-500 hover:border-white hover:cursor-pointer flex items-center w-min px-3 py-1 mt-4 rounded-2xl">
+            <Public />
+            <h1 className="font-semibold text-sm">English</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavComponent2;
