5e7e9b816b356c56138c2488824c3639d3897b3a
dhruv8433 <dhruvrsoni04@gmail.com>
2024-01-09 21:42:23 +0530

    config

---
+++ app/config/config.js
@@ -0,0 +1,4 @@
+export const api_key = "b247c0a6155322b3b700a957adc0d33c";
+
+export const clientId = "577b9546881c4879986addfff077f336";
+export const clientSecret = "cf2aa24b101b4d61be654369a7f4d7c7";
