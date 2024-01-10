6dedf1cea5f82affd66f4e8ce79696f616ef69f9
dhruv8433 <dhruvrsoni04@gmail.com>
2024-01-10 09:26:14 +0530

    home

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -5,16 +5,21 @@ import { fetchSongs } from "../service/fetchSongs";
 import SongCard from "./SongCard";
 import { getAccessToken } from "../service/getAccessToken";
 import { Grid } from "@mui/material";
+import Skeleton from "./Skeleton";
+import SkeletonCard from "./Skeleton";
 
 const Home = () => {
   const [songs, setSongs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   async function getSongs() {
     try {
+      // promise -> only after getting token from spotify send request to fetching playlists
       getAccessToken().then(async (respnose) => {
         const song = await fetchSongs(respnose);
         console.log(song);
         setSongs(song.playlists.items);
+        setLoading(false);
       });
     } catch (error) {
       console.log("error in getting tracks", error);
@@ -32,15 +37,48 @@ const Home = () => {
         <h1 className="font-semibold text-gray-400 text-sm">Show all</h1>
       </div>
       <Grid container className="flex mt-4">
-        {/* getting only first 7 songs using slice */}
-        {songs.slice(0, 7).map((song) => (
-          <Grid key={song.id} item xs={12} md={2}>
-            <SongCard key={song.id} song={song} />
-          </Grid>
-        ))}
+        {/* if response is loading than show skeleton */}
+        {loading ? (
+          <SkeletonContainer />
+        ) : (
+          // only fetching first 7 response from spotify api
+          songs.slice(0, 7).map((song) => (
+            <Grid key={song.id} item xs={4} md={2}>
+              <SongCard key={song.id} song={song} />
+            </Grid>
+          ))
+        )}
       </Grid>
     </div>
   );
 };
 
 export default Home;
+
+const SkeletonContainer = () => {
+  return (
+    <Grid container>
+      <Grid item xs={4} md={2}>
+        <SkeletonCard />
+      </Grid>
+      <Grid item xs={4} md={2}>
+        <SkeletonCard />
+      </Grid>
+      <Grid item xs={4} md={2}>
+        <SkeletonCard />
+      </Grid>
+      <Grid item xs={4} md={2}>
+        <SkeletonCard />
+      </Grid>
+      <Grid item xs={4} md={2}>
+        <SkeletonCard />
+      </Grid>
+      <Grid item xs={4} md={2}>
+        <SkeletonCard />
+      </Grid>
+      <Grid item xs={4} md={2}>
+        <SkeletonCard />
+      </Grid>
+    </Grid>
+  );
+};
