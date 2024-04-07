d0ae350c919aabd3cd004a8c672bdb165530ffe6
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-04-07 15:35:50 +0530

    global layout

--- app/layout.js
+++ app/layout.js
@@ -6,39 +6,44 @@ import "./globals.css";
 import { Grid } from "@mui/material";
 import SecondaryNavbar from "./components/SecondaryNavbar";
 import Box from "@mui/material/Box";
+import BottomPlayer from "./components/BottomPlayer";
+import StoreProvider from "./StoreProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Spotify - Web Player: Music for everyone"
+  title: "Spotify - Web Player: Music for everyone",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Grid container>
-          <Grid item xs={12} md={3}>
-            {/* navigation bar */}
-            <Box display={{ xs: "none", md: "block" }}>
-              <PrimaryNavbar />
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={9}>
-            <div className="secondary-nav mx-2 rounded-md">
-              {/* secondary nav */}
-              <SecondaryNavbar />
+        <StoreProvider>
+          <Grid container>
+            <Grid item xs={12} md={3}>
+              {/* navigation bar */}
+              <Box display={{ xs: "none", md: "block" }}>
+                <PrimaryNavbar />
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={9}>
+              <div className="secondary-nav mx-2 rounded-md">
+                {/* secondary nav */}
+                <SecondaryNavbar />
 
-              {/* childrens */}
-              {children}
+                {/* childrens */}
+                {children}
 
-              {/* footer component */}
-              <div>
-                <Footer />
+                {/* footer component */}
+                <div>
+                  <Footer />
+                </div>
               </div>
-            </div>
+            </Grid>
           </Grid>
-        </Grid>
+          <BottomPlayer />
+        </StoreProvider>
       </body>
     </html>
   );
