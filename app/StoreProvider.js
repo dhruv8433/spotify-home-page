fbf31cc931a6a8ce60d7ad572ab8002d93adb79e
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-04-07 15:36:03 +0530

    store provider

---
+++ app/StoreProvider.js
@@ -0,0 +1,19 @@
+"use client"
+
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store";
+
+import React from "react";
+
+const StoreProvider = ({ children }) => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
+};
+
+export default StoreProvider;
