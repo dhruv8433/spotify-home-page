8a12bbcfd0e177114259d408ed85519cad56eddb
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-04-07 15:36:35 +0530

    playlist song card

--- app/components/PlaylistSongCard.jsx
+++ app/components/PlaylistSongCard.jsx
@@ -1,6 +1,8 @@
-import { TableCell, TableRow } from "@mui/material";
 import React from "react";
 import moment from "moment";
+import { useDispatch } from "react-redux";
+import { TableCell, TableRow } from "@mui/material";
+import { PlaySong } from "../action/action";
 
 const PlaylistSongCard = ({ song, index }) => {
   function formatDuration(duration_ms) {
@@ -8,10 +10,18 @@ const PlaylistSongCard = ({ song, index }) => {
     let seconds = ((duration_ms % 60000) / 1000).toFixed(0);
     return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
   }
+
+  const dispatch = useDispatch();
+  function setAlbumSong(song) {
+    dispatch(PlaySong(song));
+  }
   return (
     <React.Fragment key={index}>
-      <TableRow hover className="hover:cursor-pointer">
-        <TableCell>
+      <TableRow
+        onClick={() => setAlbumSong(song)}
+        className="hover:cursor-pointer"
+      >
+        <TableCell className="">
           <h1 className="text-white">{index + 1}</h1>
         </TableCell>
         <TableCell>
@@ -22,14 +32,26 @@ const PlaylistSongCard = ({ song, index }) => {
               height={50}
               width={50}
             />
+            {song.track.preview_url && (
+              <audio controls>
+                <source src={song.track.preview_url} type="audio/mpeg" />
+                Your browser does not support the audio element.
+              </audio>
+            )}
             <div className="ml-2">
-              <h1 className="font-semibold text-white">{song.track.name}</h1>
-              <p className="text-white">{song.track.artists[0]?.name}</p>
+              <h1 className="font-semibold text-white hover:underline">
+                {song.track.name}
+              </h1>
+              <p className="text-white hover:underline">
+                {song.track.artists[0]?.name}
+              </p>
             </div>
           </div>
         </TableCell>
         <TableCell>
-          <h1 className="text-gray-500">{song.track.album.name}</h1>
+          <h1 className="text-gray-500 hover:underline">
+            {song.track.album.name}
+          </h1>
         </TableCell>
         <TableCell>
           <h1 className="text-gray-500">{moment(song.added_at).fromNow()}</h1>
