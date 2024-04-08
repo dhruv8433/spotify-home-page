c60320631cb69675254740fb0caa207e148c57c5
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-04-08 12:03:49 +0530

    song playlist card

--- app/components/PlaylistSongCard.jsx
+++ app/components/PlaylistSongCard.jsx
@@ -1,28 +1,79 @@
-import React from "react";
+"use client";
+import React, { useRef, useState } from "react";
 import moment from "moment";
 import { useDispatch } from "react-redux";
 import { TableCell, TableRow } from "@mui/material";
 import { PlaySong } from "../action/action";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
 
 const PlaylistSongCard = ({ song, index }) => {
-  function formatDuration(duration_ms) {
+  const [hoveredTrack, setHoveredTrack] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [playingTrack, setPlayingTrack] = useState(null);
+  const audioRef = useRef(new Audio());
+  const dispatch = useDispatch();
+
+  const handleMouseEnter = (trackId) => {
+    setHoveredTrack(trackId);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredTrack(null);
+  };
+
+  const handlePlayPause = () => {
+    const audio = audioRef.current;
+    if (playingTrack === song.track.id) {
+      if (isPlaying) {
+        audio.pause();
+        setIsPlaying(false);
+      } else {
+        audio.play();
+        setIsPlaying(true);
+      }
+    } else {
+      if (audio.src !== song.track.preview_url) {
+        audio.src = song.track.preview_url;
+      }
+      audio.play();
+      setIsPlaying(true);
+      setPlayingTrack(song.track.id);
+      dispatch(PlaySong(song));
+    }
+  };
+
+  const formatDuration = (duration_ms) => {
     let minutes = Math.floor(duration_ms / 60000);
     let seconds = ((duration_ms % 60000) / 1000).toFixed(0);
     return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
-  }
+  };
 
-  const dispatch = useDispatch();
-  function setAlbumSong(song) {
-    dispatch(PlaySong(song));
-  }
   return (
     <React.Fragment key={index}>
       <TableRow
-        onClick={() => setAlbumSong(song)}
+        onMouseEnter={() => handleMouseEnter(song.track.id)}
+        onMouseLeave={handleMouseLeave}
         className="hover:cursor-pointer"
       >
         <TableCell className="">
-          <h1 className="text-white">{index + 1}</h1>
+          {hoveredTrack ? (
+            <h1 key={song.track.id} className="text-white">
+              {(hoveredTrack === song.track.id ||
+                playingTrack === song.track.id) &&
+              song.track.preview_url ? (
+                playingTrack === song.track.id ? (
+                  <PauseIcon onClick={handlePlayPause} fontSize="small" />
+                ) : (
+                  <PlayArrowIcon onClick={handlePlayPause} fontSize="small" />
+                )
+              ) : (
+                <PlayArrowIcon onClick={handlePlayPause} fontSize="small" />
+              )}
+            </h1>
+          ) : (
+            <h1 className="text-white">{index + 1}</h1>
+          )}
         </TableCell>
         <TableCell>
           <div className="flex items-center">
@@ -32,12 +83,6 @@ const PlaylistSongCard = ({ song, index }) => {
               height={50}
               width={50}
             />
-            {song.track.preview_url && (
-              <audio controls>
-                <source src={song.track.preview_url} type="audio/mpeg" />
-                Your browser does not support the audio element.
-              </audio>
-            )}
             <div className="ml-2">
               <h1 className="font-semibold text-white hover:underline">
                 {song.track.name}
