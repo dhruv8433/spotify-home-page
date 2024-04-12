ca83b4103984fb3ecffd8e012f4a25f3d7055577
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-04-12 19:27:26 +0530

    root reducers

--- app/reducers/rootReducer.js
+++ app/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { songReducer } from "./songReducer";
+import authReducer from "./authReducer";
 
 const rootReducer = combineReducers({
   songReducer: songReducer,
+  auth: authReducer,
 });
 
 export default rootReducer;
