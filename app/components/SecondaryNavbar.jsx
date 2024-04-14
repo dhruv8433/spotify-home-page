4cc35a12a158584a0135c6c5b29bed3b8766e143
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-04-14 19:39:39 +0530

    navbar login model

--- app/components/SecondaryNavbar.jsx
+++ app/components/SecondaryNavbar.jsx
@@ -1,8 +1,13 @@
+"use client";
+
 import { NavigateBeforeOutlined, NavigateNext } from "@mui/icons-material";
+import { Modal } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
-import React from "react";
+import React, { useState } from "react";
+import LoginModel from "../Model/loginModel";
 
 const SecondaryNavbar = () => {
+  const [loginModelOpen, setLoginModelOpen] = useState(false);
   return (
     <div className="px-2">
       <div className="mt-2 flex justify-between">
@@ -19,11 +24,22 @@ const SecondaryNavbar = () => {
         {/* login buttons */}
         <div className="flex items-center p-2 justify-between">
           <button className="text-gray-300 font-semibold mr-6">Sign up</button>
-          <button className="px-6 font-semibold py-2 bg-white rounded-3xl text-black">
+          <button
+            onClick={() => setLoginModelOpen(true)}
+            className="px-6 font-semibold py-2 bg-white rounded-3xl text-black"
+          >
             Login
           </button>
         </div>
       </div>
+      {/* Login Model */}
+      <>
+        <Modal open={loginModelOpen} onClose={() => setLoginModelOpen(false)}>
+          <div className="">
+            <LoginModel />
+          </div>
+        </Modal>
+      </>
     </div>
   );
 };
