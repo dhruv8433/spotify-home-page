e359e962380e79db5034c8efb76ae41f7c99424a
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-04-14 19:39:42 +0530

    login model

---
+++ app/Model/LoginModel.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoginModel = () => {
+  return (
+    <div className="flex justify-center items-center h-full p-4 bg-black rounded-md">
+      Hlo
+    </div>
+  );
+};
+
+export default LoginModel;
